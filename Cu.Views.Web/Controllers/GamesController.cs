264b7d6521c82dbff89e03d43bb1c2a9d75b428c
Mileto Di Marco <mileto.di.marco@howest.be>
2025-10-14 16:29:36 +0200

    Add project files.

---
+++ Cu.Views.Web/Controllers/GamesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cu.Views.Web.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewData["PageTitle"] = "Games";
+            ViewBag.PageTitle = "Games";
+            return View();
+        }
+    }
+}
