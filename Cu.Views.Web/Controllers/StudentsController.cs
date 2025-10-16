28b5e4815e2e020db0865e1959c02d54a086f6c7
Mileto Di Marco <mileto.di.marco@howest.be>
2025-10-16 08:42:34 +0200

    Add student repo

---
+++ Cu.Views.Web/Controllers/StudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cu.Views.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
